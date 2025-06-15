fa542e1d5b6cf23294446a301d9ed39742bde2a7
saurabhaidm <saurabh_aidm@alpixn.com>
2025-06-15 12:51:03 +0530

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/home/Home'
+import Dashboard from './pages/dashboard/Dashboard'
 
 function App() {
   
 
   return (
     <div>
-         Hello! this is AutoMark Ai.
+        <BrowserRouter>
+            <Routes>
+               <Route path='/'  element={ <Home/>  }  />
+               <Route path='/dashboard'  element={ <Dashboard/> }  />
+               
+            </Routes>
+        </BrowserRouter>
     </div>
   )
 }
